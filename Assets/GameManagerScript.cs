996dd689aa7ed813018a5a4e5a35bc12f639d2a7
maenotakanori <115054954+maenotakanori@users.noreply.github.com>
2024-04-22 13:08:29 +0900

    a

--- Assets/GameManagerScript.cs
+++ Assets/GameManagerScript.cs
@@ -5,70 +5,88 @@ using UnityEngine;
 public class GameManagerScript : MonoBehaviour
 {
     // Start is called before the first frame update
-    
-    int[] map;
+
+    public GameObject playerPrefab;
+    int[,] map; //レベルデザイン用の配列
+    GameObject[,] field; // ゲーム完了用の配列
 
     // Start is called before the first frame update
     void Start()
     {
-        map = new int[] { 0, 0, 0, 1, 0, 2, 0, 0, 0, };
-        PrintArray();
-        string debugText = "";
-        for (int i = 0; i < map.Length; i++)
+        map = new int[,] {
+           {0,0,0,0,0 },
+           {0,0,1,0,0 },
+           {0,0,0,0,0 },
+        };
+        field = new GameObject
+            [
+             map.GetLength(0),
+             map.GetLength(1)
+            ];
+        for(int y = 0; y < map.GetLength(0); y++)
         {
-            debugText += map[i].ToString() + ",";
-        }
-        Debug.Log(debugText);
-    }
-
-     // Update is called once per frame
-     void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.RightArrow))
-        {
-            int playerIndex = GetPlayerIndex();
-
-            MoveNumber(1, playerIndex, playerIndex + 1);
-            PrintArray();
-        }
-     }
-     void PrintArray()
-    {
-        string debugText = "";
-        for (int i = 0; i < map.Length; i++)
-        {
-            debugText += map[i].ToString() + ",";
-        }
-            Debug.Log(debugText);
-     }
-     int GetPlayerIndex()
-    {
-        for(int i = 0;i < map.Length; i++)
-        {
-            if (map[i] == 1)
+            for(int x = 0; x < map.GetLength(1); x++)
             {
-                return i;
+                if (map[y,x] == 1)
+                {
+                    field[y,x] = Instantiate(
+                    playerPrefab,
+                    new Vector3(x, map.GetLength(0) - 1 - y, 0),
+                    Quaternion.identity
+                  );
+                }
             }
         }
-            return -1;
+    }
+
+    // Update is called once per frame
+    //void Update()
+    //{
+    //   if (Input.GetKeyDown(KeyCode.RightArrow))
+    //   {
+    //       //int playerIndex = GetPlayerIndex();
+            //MoveNumber(1, playerIndex, playerIndex + 1);
+            //PrintArray();
+    //   }
+    //}
+     void PrintArray()
+    {
+    //    string debugText = "";
+    //    for (int i = 0; i < map.Length; i++)
+    //    {
+    //        debugText += map[i].ToString() + ",";
+    //    }
+    //        Debug.Log(debugText);
+     }
+    Vector2Int GetPlayerIndex()
+    {
+        for (int y = 0; y < field.GetLength(0); y++)
+        {
+            for (int x = 0; x < field.GetLength(1); x++)
+            {
+                if (field[y, x] == null) { continue; }
+                if (field[y, x].tag == "Player") { return new Vector2Int(x, y); }
+            }
+        }
+            return new Vector2Int(-1,-1);
      }
      bool MoveNumber(int number,int moveFrom,int moveTo)
         {
         //移動可能か判断  
         if (moveTo < 0 || moveTo >= map.Length){return false;}
         //移動先に箱があったら
-        if (map[moveTo] == 2)
-        {
+        //if (map[moveTo] == 2)
+        //{
             //移動方向算出
             int velocity = moveTo - moveFrom;
             //playerの移動方向へ箱を移動させる
             bool success = MoveNumber(2, moveTo, moveTo + velocity);
             //箱の移動が失敗したらplayerの移動も失敗
             if (success　== false) { return false; }
-        }
+       // }
         //移動
-        map[moveTo] = number;
-        map[moveFrom] = 0;
+       // map[moveTo] = number;
+       // map[moveFrom] = 0;
         return true;
     }
     
